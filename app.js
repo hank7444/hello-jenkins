ea1031d73bc0ef47728469aa7a27bff169f8291f
Hank Kuo <hank_kuo@hiiir.com>
2015-02-24 17:53:40 +0800

    test deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 // 1235
 app.get('/', function (req, res) {
-  res.send('hello jenkins');
+  res.send('hello jenkins test deploy');
 });
 
 app.listen(process.env.PORT || 5000);

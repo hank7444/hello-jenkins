f8df5baf92d47714846ad45636d1fcfd2443defe
hank7444 <hank7444@gmail.com>
2015-02-24 04:27:38 -0500

    test ok

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 // 1235
 app.get('/', function (req, res) {
-  res.send('hello jenkins test');
+  res.send('hello jenkins');
 });
 
 app.listen(process.env.PORT || 5000);
